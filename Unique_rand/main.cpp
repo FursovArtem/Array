31811d9bac571b5d35aa5e4bcef34a7e35b5f602
Artem <aztec@DESKTOP-CK171NA>
2023-05-30 17:35:41 +0400

    unique

---
+++ Unique_rand/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n]{};
+	for (int i = 0; i < 10; i++)
+	{
+		bool unique;
+		do {
+			arr[i] = rand() % 10;
+			unique = true;
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;
+				}
+			}
+		} while (!unique);
+		cout << arr[i] << tab;
+	}
+}
